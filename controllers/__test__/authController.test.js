01935b8d7965d2b681cd08a58e0b806fe8924458
KaterynaBachkalo <kozlovets88@gmail.com>
2024-01-08 22:37:15 +0200

    correction tests

--- controllers/__test__/authController.test.js
+++ controllers/__test__/authController.test.js
@@ -1,25 +1,26 @@
 const request = require("supertest");
-
+const { expect, describe, it, afterAll } = require("@jest/globals");
+const mongoose = require("mongoose");
 const app = require("../../server");
 
-describe("test POST /auth/login", () => {
-  it("should return status 200", async () => {
-    const testData = {
-      email: "Kate@mail.com",
-      password: "A@123456a",
-    };
-    const res = await request(app).post("/api/users/login").send(testData);
+const testData = {
+  email: "Kate@mail.com",
+  password: "A@123456a",
+};
 
+describe("test POST /auth/login", () => {
+  afterAll(() => {
+    mongoose.disconnect();
+    app.close();
+  });
+
+  it("should return status 200", async () => {
+    const res = await request(app).post("/api/users/login").send(testData);
     expect(res.statusCode).toBe(200);
   });
 
   it("should return token", async () => {
-    const testData = {
-      email: "Kate@mail.com",
-      password: "A@123456a",
-    };
     const res = await request(app).post("/api/users/login").send(testData);
-
     expect(res.body).toEqual(
       expect.objectContaining({
         token: expect.any(String),
@@ -28,12 +29,7 @@ describe("test POST /auth/login", () => {
   });
 
   it("should return obj user with 2 fields email and subscription with type String", async () => {
-    const testData = {
-      email: "Kate@mail.com",
-      password: "A@123456a",
-    };
     const res = await request(app).post("/api/users/login").send(testData);
-
     expect(res.body).toEqual(
       expect.objectContaining({
         user: { email: expect.any(String), subscription: expect.any(String) },
